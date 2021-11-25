af482ef2d933b42a36203aa0c28aa29445961510
Wesley Klop <wesley19097@gmail.com>
2021-11-25 10:56:24 +0100

    Add prisma client

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import { PrismaClient } from '../prisma.mjs'
+
+const dbClient = new PrismaClient()
 
 const router = Router()
 
-router.get('/', function (req, res, next) {
-    res('yolo')
+router.get('/', async (req, res, next) => {
+    const users = await dbClient.users.findMany()
+    res.json(users)
 })
 
 export default router
