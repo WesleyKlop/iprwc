db5453378f4cf0ae311b2cebfbfb087acae0a77e
Wesley Klop <wesley19097@gmail.com>
2021-12-17 14:41:26 +0100

    Close navbar after navigation

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, ContentChild, Input } from '@angular/core'
+import { NavigationStart, Router } from '@angular/router'
+import { filter } from 'rxjs'
 import { NavbarDirective } from './navbar.directive'
+
+const filterNavigationStart = (event: any): event is NavigationStart => {
+  return event instanceof NavigationStart
+}
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +13,12 @@ import { NavbarDirective } from './navbar.directive'
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  constructor(router: Router) {
+    router.events.pipe(filter(filterNavigationStart)).subscribe(() => {
+      this.isOpen = false
+    })
+  }
+
   @Input()
   title!: string
 
